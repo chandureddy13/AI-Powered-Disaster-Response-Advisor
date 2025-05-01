a5dd6d78f27d67190f7c599c2acb16430533dacb
K. Chandrakanth Reddy <chanduredy2579@gmail.com>
2025-05-01 22:30:06 +0530

    deng

---
+++ s.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = "CHANDU IS THE KING OF THE HIS OWN THOUGHTS",
+    layout = "centered",
+    initial_sidebar_state = "expanded",
+    page_icon = "🦁",
+    
+)
